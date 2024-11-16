ecf81d0a8c70e7db6f44d2f3edaa66401c35b508
Guido <gvm0898@gmail.com>
2024-11-16 20:01:59 +0100

    home carga los deportes

--- frontend/ZenithZone/src/views/client/Home.vue
+++ frontend/ZenithZone/src/views/client/Home.vue
@@ -1,5 +1,28 @@
 <template>
     <h1>Home</h1>
+    <ul>
+        <li v-for="deporte in state.deportes" :key="deporte.id">{{ deporte.nombre }}</li>
+    </ul>
 </template>
+
+<script>
+import { reactive, computed } from 'vue';
+import { useStore } from 'vuex';
+import Constant from '../../Constant';
+
+export default {
+    setup() {
+        const store = useStore();
+
+        store.dispatch(`deportes/${Constant.INITIALIZE_DEPORTE}`);
+
+        const state = reactive({
+            deportes: computed(() => store.getters['deportes/GetDeportes'])
+        });
+
+        return { state };
+    }
+}
+</script>
 
 <style></style>
