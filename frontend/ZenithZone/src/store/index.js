502d85ba0d05de906e0947e406da0ee08152fc6b
Guido <gvm0898@gmail.com>
2024-11-16 20:01:38 +0100

    store structura + deportes store

--- frontend/ZenithZone/src/store/index.js
+++ frontend/ZenithZone/src/store/index.js
@@ -1,5 +1,8 @@
 import Vuex from "vuex";
+import { deportes } from "./modules/client/deportes";
 
 export default Vuex.createStore({
-    modules: {}
+    modules: {
+        deportes: deportes
+    }
 });
