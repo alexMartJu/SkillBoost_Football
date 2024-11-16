33adabefd2d8cd8f2226bf792676c1c6aea52baa
Guido <gvm0898@gmail.com>
2024-11-16 03:11:27 +0100

    arreglado el main.js quitando algunas cosas

--- frontend/ZenithZone/src/main.js
+++ frontend/ZenithZone/src/main.js
@@ -3,15 +3,6 @@ import App from './App.vue';
 import router from './router/router';
 import store from './store';
 import VueLazyLoad from 'vue3-lazyload';
-import Swal from 'sweetalert2';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import './assets/main.scss';
-
-// Iconos
-import { faHeart, faComment } from '@fortawesome/free-solid-svg-icons';
-library.add(faHeart);
-library.add(faComment);
 
 const app = createApp(App);
 
@@ -22,9 +13,6 @@ app.use(VueLazyLoad, {
     loading: '/path/to/loading-image.png',
     error: '/path/to/error-image.png',
 });
-
-// Componentes
-app.component('font-awesome-icon', FontAwesomeIcon);
 
 // Montar la app
 app.mount('#app');
