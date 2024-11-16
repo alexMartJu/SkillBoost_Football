6f5d72df94346eabc5bc55bbb4aaed1f1ea926c6
Guido <gvm0898@gmail.com>
2024-11-16 04:08:53 +0100

    añadido el active class al header

--- frontend/ZenithZone/src/components/Header.vue
+++ frontend/ZenithZone/src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="d-flex justify-content-between align-items-center p-3">
             <!-- Logo or App Name -->
-            <div class="text-white h4 mb-0">
+            <div class="text-white h2 mb-0">
                 ZenithZone
             </div>
 
@@ -11,12 +11,12 @@
             <nav>
                 <ul class="nav">
                     <li class="nav-item">
-                        <router-link to="/home" class="nav-link text-white hover:text-light">
+                        <router-link to="/home" class="nav-link text-white fw-bold fs-5" active-class="isActive">
                             Home
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link to="/instalaciones" class="nav-link text-white hover:text-light">
+                        <router-link to="/instalaciones" class="nav-link text-white fw-bold fs-5" active-class="isActive">
                             Instalaciones
                         </router-link>
                     </li>
@@ -32,7 +32,11 @@ export default {
     name: "Header",
 };
 </script>
- 
+
+
 <style scoped>
-/* Optional: Custom styles if needed */
+.isActive {
+    background-color: #426fa0;
+    border-radius: 5px;
+}
 </style>
