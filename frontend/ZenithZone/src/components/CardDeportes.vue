3b7890fcc61cb7724aa15d4b430796feb5016b07
Guido <gvm0898@gmail.com>
2024-11-18 02:40:33 +0100

    Frontend. Estructuración de los datos en las CardDeportes

--- frontend/ZenithZone/src/components/CardDeportes.vue
+++ frontend/ZenithZone/src/components/CardDeportes.vue
@@ -3,11 +3,23 @@
         <h1>{{ deporte.nombre }}</h1>
         <h3>{{ deporte.slug }}</h3>
         <strong>pistas</strong>
-        <p>{{ deporte.pistas }}</p>
+        <ul>
+            <li v-for="pista in deporte.pistas" :key="pista.id">{{ pista.nombre }}</li>
+        </ul>
         <strong>clases</strong>
-        <p>{{ deporte.clases }}</p>
+        <ul>
+            <li v-for="clase in deporte.clases" :key="clase.id">
+                <p><strong>Nombre:</strong> {{ clase.nombre }}</p>
+                <p><strong>Descripción:</strong> {{ clase.descripcion }}</p>
+                <p><strong>Duración:</strong> {{ clase.duracion }} minutos</p>
+                <p><strong>Plazas totales:</strong> {{ clase.maxPlazas }}</p>
+                <p><strong>Precio:</strong> {{ clase.precio }}€</p>
+            </li>
+        </ul>
     </div>
 </template>
+
+
 
 <script>
 export default {
