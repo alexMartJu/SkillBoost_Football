e0523b1fe00808146bd69370a6875d540a2e75a4
alex <alex.mart.juan@gmail.com>
2024-11-20 23:09:14 +0100

    FrontEnd. Creado service entrenadorDashboard (listar/crear entrenamientos).

---
+++ frontend/ZenithZone/src/services/dashboards/entrenadorDashboard.service.js
@@ -0,0 +1,14 @@
+import secrets from "@/secrets";
+import Api from "../Api";
+
+export default {
+    // Obtener la lista de entrenamientos desde la API
+    GetEntrenamientos() {
+        return Api(secrets.URL_LARAVEL).get('entrenamientos');
+    },
+
+    // Crear un nuevo entrenamiento en la API
+    CreateEntrenamiento(nuevoEntrenamiento) {   
+        return Api(secrets.URL_LARAVEL).post('entrenamientos', nuevoEntrenamiento);
+    }
+};
