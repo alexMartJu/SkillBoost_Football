3caf1e44c481c0fbdca71386117a1825790879f0
Guido <gvm0898@gmail.com>
2024-11-21 01:58:34 +0100

    Frontend. Pequeñas correcciones de diseño a footer y header

--- frontend/ZenithZone/src/components/Footer.vue
+++ frontend/ZenithZone/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-<footer class="bg-dark text-white py-4">
+<footer class="footer text-white py-5">
     <div class="container text-center">
         <!-- Texto o información de derechos de autor -->
         <p class="mb-0">
@@ -25,11 +25,6 @@
 <script>
 export default {
     name: 'Footer',
-    data() {
-        return {
-            currentYear: new Date().getFullYear(),
-        };
-    },
 };
 </script>
 
@@ -38,5 +33,10 @@ export default {
 /* Estilos opcionales personalizados */
 .social-links i {
     font-size: 20px;
+}
+
+.footer {
+    margin-top: 50px;
+    background-color: rgb(20, 20, 20);
 }
 </style>
--- frontend/ZenithZone/src/components/Header.vue
+++ frontend/ZenithZone/src/components/Header.vue
@@ -3,39 +3,39 @@
         <div class="container-fluid">
             <div class="d-flex justify-content-between align-items-center p-3">
                 <!-- Logo or App Name -->
-                <router-link to="/home" class="text-color fw-bold h2 mb-0">
+                <router-link to="/home" class="text-color fw-bold h2 mb-0 ms-3">
                     <img src="../assets/logo_2.png" alt="" class="logo">
                 </router-link>
 
                 <!-- Navigation Links -->
                 <nav>
                     <ul class="nav me-5">
-                        <li class="nav-item me-2">
-                            <router-link to="/home" class="nav-link text-color fw-bold fs-4"
+                        <li class="nav-item me-3">
+                            <router-link to="/home" class="nav-link text-color fw-bold fs-5"
                                 active-class="isActive text-black">
                                 Home
                             </router-link>
                         </li>
-                        <li class="nav-item me-2">
-                            <router-link to="/instalaciones" class="nav-link text-color fw-bold fs-4"
+                        <li class="nav-item me-3">
+                            <router-link to="/instalaciones" class="nav-link text-color fw-bold fs-5"
                                 active-class="isActive text-black">
                                 Instalaciones
                             </router-link>
                         </li>
-                        <li class="nav-item me-2">
-                            <router-link to="/Servicios" class="nav-link text-color fw-bold fs-4"
+                        <li class="nav-item me-3">
+                            <router-link to="/Servicios" class="nav-link text-color fw-bold fs-5"
                                 active-class="isActive text-black">
                                 Servicios
                             </router-link>
                         </li>
-                        <li class="nav-item me-5">
-                            <router-link to="/entrenadores" class="nav-link text-color fw-bold fs-4"
+                        <li class="nav-item me-4">
+                            <router-link to="/entrenadores" class="nav-link text-color fw-bold fs-5"
                                 active-class="isActive text-black">
                                 Entrenadores
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link to="/auth" class="nav-link auth fw-bold fs-4"
+                            <router-link to="/auth" class="nav-link auth fw-bold fs-5"
                                 active-class="isActive text-black">
                                 Unirse al club
                             </router-link>
@@ -73,7 +73,6 @@ export default {
     background-color: white;
     border-radius: 10px;
     color: #ff6600;
-    padding: 10px 25px;
     transition: all 0.3s;
 
     &:hover {
@@ -87,7 +86,7 @@ export default {
 }
 
 .logo {
-    max-height: 70px;
+    max-height: 50px;
     width: auto;
     transition: transform 0.3s;
 
