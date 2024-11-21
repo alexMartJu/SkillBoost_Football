113bd50e450ef8568321697ba372a64ab9f4f17b
Rafajorda <rafajorgis@gmail.com>
2024-11-21 16:00:26 +0100

    creacion pistas y deportes en /admin

--- frontend/ZenithZone/src/services/dashboards/adminDashboard.service.js
+++ frontend/ZenithZone/src/services/dashboards/adminDashboard.service.js
@@ -5,7 +5,13 @@ export default {
     GetDeportes() {
         return Api(secrets.URL_LARAVEL).get('deportes');
     },
+    CreateDeporte(deporte) {
+        return Api(secrets.URL_LARAVEL).post('deportes', deporte);
+      },
     GetPistas(){
         return Api(secrets.URL_LARAVEL).get('pistas');
-    }
+    },
+    CreatePista(pista) {
+        return Api(secrets.URL_LARAVEL).post('pistas', pista);
+      },
 }
