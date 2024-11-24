d9a165ab882fd7bc32ba1931ef40b3e81d32380f
alex <alex.mart.juan@gmail.com>
2024-11-24 16:09:26 +0100

    Backend_Springboot. Modificado DeporteController para obtener las pistas de un deporte.

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/controller/DeporteController.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/controller/DeporteController.java
@@ -1,8 +1,10 @@
 package com.polideportivo.backend_springboot.api.controller;
 
 import com.polideportivo.backend_springboot.api.assembler.DeporteAssembler;
+import com.polideportivo.backend_springboot.api.assembler.PistaAssembler;
 import com.polideportivo.backend_springboot.api.model.deporte.DeporteResponse;
 import com.polideportivo.backend_springboot.api.model.deporte.DeporteWrapper;
+import com.polideportivo.backend_springboot.api.model.pista.PistaWrapper;
 import com.polideportivo.backend_springboot.domain.service.DeporteService;
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +20,7 @@ public class DeporteController {
 
     private final DeporteService deporteService;
     private final DeporteAssembler deporteAssembler;
+    private final PistaAssembler pistaAssembler;
     
     // Obtener todos los deportes
     @GetMapping("/deportes")
@@ -33,5 +36,15 @@ public class DeporteController {
     public DeporteResponse getBySlug(@PathVariable String slug) {
         var deporte = deporteService.getBySlug(slug);
         return deporteAssembler.toResponse(deporte);
+    }
+
+    // Obtener las pistas de un deporte
+    @GetMapping("/deportes/{slug}/pistas")
+    public PistaWrapper getPistasByDeporte(@PathVariable String slug) {
+        var deporte = deporteService.getBySlug(slug); // Busca por slug
+        var pistas = deporte.getPistas();
+        System.out.println(pistas);
+
+        return pistaAssembler.toCollectionModel(pistas);
     }
 }
