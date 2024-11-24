141ccdf16dc66c1a59f2f296b4dedaa50af88e84
alex <alex.mart.juan@gmail.com>
2024-11-24 16:01:02 +0100

    Backend_Springboot. Creados PistaService y PistaServiceImpl para obtener todas las pistas.

---
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/domain/service/PistaService.java
@@ -0,0 +1,10 @@
+package com.polideportivo.backend_springboot.domain.service;
+
+import com.polideportivo.backend_springboot.domain.model.Pista;
+
+import java.util.List;
+
+public interface PistaService {
+    
+    List<Pista> getAllPistas();
+}
---
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/domain/service/PistaServiceImpl.java
@@ -0,0 +1,20 @@
+package com.polideportivo.backend_springboot.domain.service;
+
+import com.polideportivo.backend_springboot.domain.model.Pista;
+import com.polideportivo.backend_springboot.domain.repository.PistaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PistaServiceImpl implements PistaService {
+    private final PistaRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Pista> getAllPistas() {
+        return repository.findAll();
+    }
+}
