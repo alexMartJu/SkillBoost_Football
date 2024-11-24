67b5b8906748dca21e4aa1d7a6c626c86d2adba1
alex <alex.mart.juan@gmail.com>
2024-11-24 15:19:34 +0100

    Backend_SpringBoot. Borrado de archivos de antigua estructura para añadir nueva.

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/repository/DeporteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.polideportivo.backend_springboot.repository;
-
-import com.polideportivo.backend_springboot.model.Deporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeporteRepository extends JpaRepository<Deporte, Long> {
-    
-}
--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/repository/PistaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.polideportivo.backend_springboot.repository;
-
-import com.polideportivo.backend_springboot.model.Pista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PistaRepository extends JpaRepository<Pista, Long> {
-    
-}
