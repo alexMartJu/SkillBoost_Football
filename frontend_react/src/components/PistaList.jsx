15916aad04f86d34f5f5b766526624991ef22113
Rafajorda <rafajorgis@gmail.com>
2024-11-25 19:19:55 +0100

    react1

---
+++ frontend_react/src/components/PistaList.jsx
@@ -0,0 +1,43 @@
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PistaList = () => {
+    const [pistas, setPistas] = useState([]); 
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        // Función para obtener deportes desde el backend
+        const fetchPistas = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/pistas');
+                setPistas(response.data); 
+                setLoading(false); 
+            } catch (error) {
+                console.error('Error fetching pistas:', error);
+                setLoading(false); 
+            }
+        };
+
+        fetchPistas();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+
+    return (
+        <div>
+            <h2>Instalaciones Disponibles</h2>
+            <ul>
+                {pistas.map((pista) => (
+                    <li key={pista.id} style={{ borderBottom: '1px solid #ddd', padding: '8px 0' }}>
+                        <h3>{pista.nombre}</h3>
+                        <p>Slug: {pista.slug}</p>
+                        <img src={pista.image} alt={pista.nombre} style={{ width: '100px', height: '100px' }} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default PistaList;
