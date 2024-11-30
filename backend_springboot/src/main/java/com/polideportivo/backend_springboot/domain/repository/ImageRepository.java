3886748b02cbfa834ef9b21fde2f1000b4ef5385
alex <alex.mart.juan@gmail.com>
2024-11-30 20:12:44 +0100

    Backend_Springboot. Modificados repositories de Deporte, Image y Pista.

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/domain/repository/ImageRepository.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/domain/repository/ImageRepository.java
@@ -2,12 +2,11 @@ package com.polideportivo.backend_springboot.domain.repository;
 
 import com.polideportivo.backend_springboot.domain.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ImageRepository extends JpaRepository<Image, Long>, JpaSpecificationExecutor<Image> {
+public interface ImageRepository extends JpaRepository<Image, Long> {
     List<Image> findByImageableTypeAndImageableId(String imageableType, Long imageableId);
 }
--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/domain/repository/PistaRepository.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/domain/repository/PistaRepository.java
@@ -2,14 +2,13 @@ package com.polideportivo.backend_springboot.domain.repository;
 
 import com.polideportivo.backend_springboot.domain.model.Pista;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.List;
 
 @Repository
-public interface PistaRepository extends JpaRepository<Pista, Long>, JpaSpecificationExecutor<Pista> {
+public interface PistaRepository extends JpaRepository<Pista, Long> {
     List<Pista> findByDeletedAtIsNull();
     Optional<Pista> findBySlugAndDeletedAtIsNull(String slug);
 }
