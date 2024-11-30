8cad1a1efda8f67833835da37b726e83205e6ac3
Guido <gvm0898@gmail.com>
2024-11-30 03:36:22 +0100

    Frontend_Vue. Refactorización  de los entrenamientos para pasar todo a subcomponentes

--- frontend/ZenithZone/src/components/servicios/serviciosEntrenamientos.vue
+++ frontend/ZenithZone/src/components/servicios/serviciosEntrenamientos.vue
@@ -1,135 +1,69 @@
 <template>
-    <div class="container-fluid">        
-        <div class="text-center">
-            <filters @filters="ApplyFilters" @deleteFilters="resetFilters" :filters="filters_url" :meta="state.meta" />
-        </div>
-
-        <div>
-            <div class="row justify-content-center">
-                <CardClases 
-                    v-for="entrenamiento in state.entrenamientos" 
-                    :key="entrenamiento.id" 
-                    :entrenamiento="entrenamiento" 
-                    class="col-md-6" />
-            </div>
-        </div>
-        
-        <div class="d-flex justify-content-center mt-4">
-            <paginate 
-                v-model="state.page" 
-                :page-count="state.totalPages" 
-                :page-range="2" 
-                :margin-pages="2"
-                :click-handler="clickCallback" 
-                :prev-text="'Prev'" 
-                :next-text="'Next'" 
-                :container-class="'pagination'"
-                :page-class="'page-item'">
-            </paginate>
-        </div>
-
+<div class="container-fluid ">
+    <FiltersComponent :filters_url="state.filters" :meta="state.meta" @apply-filters="ApplyFilters" @reset-filters="resetFilters" />
+    <div class="row justify-content-center">
+        <CardClases 
+            v-for="entrenamiento in state.entrenamientos" 
+            :key="entrenamiento.id" 
+            :entrenamiento="entrenamiento" 
+            class="col-md-6" />
     </div>
+    <!-- <CardEntreamientoComponent :entrenamientos="state.entrenamientos" /> -->
+    <PaginateComponent :page="state.offset" :totalPages="state.totalPages" @update:page="updatePage" @page-click="clickCallback" />
+</div>
 </template>
 
 
 <script>
-import Filters from '@/components/filters.vue';
+import FiltersComponent from '../filters/Filters.vue';
 import CardClases from '../CardEntrenamientos.vue';
-import { useEntrenamientosData, useEntrenamientosFilters, useEntrenamientosPaginate } from '@/composables/client/useEntrenamientos';
-import { reactive } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import Paginate from 'vuejs-paginate-next';
+import PaginateComponent from '../filters/Paginate.vue';
+import {
+    useEntrenamientos
+} from '../../composables/client/useEntrenamientos';
 
 export default {
-    components: { Filters, Paginate, CardClases },
-
+    components: {
+        FiltersComponent,
+        CardClases,
+        PaginateComponent
+    },
     setup() {
-        const route = useRoute();
-        const router = useRouter();
+        const {
+            state,
+            ApplyFilters,
+            resetFilters,
+            clickCallback
+        } = useEntrenamientos();
 
-        let filters_url = {
-            nombre: "",
-            dia: "",
-            duracionMin: 0,
-            duracionMax: 0,
-            maxPlazasMin: 0,
-            maxPlazasMax: 0,
-            precioMin: 0,
-            precioMax: 0,
-            deporteId: "",
-            offset: 0,
-            limit: 4,
+        const updatePage = (pageNum) => {
+            state.offset = pageNum;
         };
 
-        const state = reactive({
-            entrenamientos: useEntrenamientosFilters(filters_url),
-            offset: filters_url.offset,
-            filters: filters_url,
-            totalPages: useEntrenamientosPaginate(filters_url.limit),
-            meta: useEntrenamientosData(),
-        });
-
-        const ApplyFilters = (filters) => {
-            const filters_64 = btoa(JSON.stringify(filters));
-            router.push({ name: 'serviciosEntrenamientosFilter', params: { filters: filters_64 } });
-            state.entrenamientos = useEntrenamientosFilters(filters);
-            state.totalPages = useEntrenamientosPaginate(filters_url.limit);
-        }
-
-        const resetFilters = () => {
-            state.filters = {
-                nombre: "",
-                dia: "",
-                duracionMin: 0,
-                duracionMax: 0,
-                maxPlazasMin: 0,
-                maxPlazasMax: 0,
-                precioMin: 0,
-                precioMax: 0,
-                deporteId: "",
-                offset: 0,
-                limit: 4,
-            };
-            ApplyFilters();
-        }
-
-        const clickCallback = (pageNum) => {
-            try {
-                if (route.params.filters !== '') {
-                    filters_url = JSON.parse(atob(route.params.filters));
-                }
-            } catch (error) {
-                console.log(error);
-            }
-            // console.log(pageNum);
-            filters_url.offset = pageNum - 1;
-            console.log(`pagenum`, pageNum);
-            state.offset = pageNum;
-            ApplyFilters(filters_url);
-        }
-
-        return { state, filters_url, ApplyFilters, resetFilters, clickCallback };
+        return {
+            state,
+            ApplyFilters,
+            resetFilters,
+            clickCallback,
+            updatePage
+        };
     }
 };
 </script>
 
+
 <style>
 .pagination {
     --bs-pagination-font-size: 20px;
-
     --bs-pagination-color: black;
     --bs-pagination-border-color: #000000;
-
     --bs-pagination-active-color: white;
     --bs-pagination-active-bg: #ff6600;
     --bs-pagination-active-border-color: #000000;
-
     --bs-pagination-hover-color: white;
     --bs-pagination-hover-bg: black;
     --bs-pagination-hover-border-color: black;
-
     --bs-pagination-disabled-border-color: lightgrey;
-
     --bs-pagination-focus-box-shadow: none;
 }
 </style>
