d0a8cb774500923c0bcb21957529144cd60e8fe9
Guido <gvm0898@gmail.com>
2024-12-01 00:28:29 +0100

    Frontend_Vue. Añadido un botón de collapse en los filtros

--- frontend/ZenithZone/src/components/filters/Filters.vue
+++ frontend/ZenithZone/src/components/filters/Filters.vue
@@ -1,7 +1,14 @@
 <template>
-<div class="text-center">
-    <filters @filters="ApplyFilters" @deleteFilters="resetFilters" :filters="filters_url" :meta="meta" />
-</div>
+    <div class="text-center">
+        <button @click="toggleCollapse">
+            {{ isCollapsed ? 'Mostrar filtros' : 'Ocultar filtros' }}
+        </button>
+        <transition name="collapse">
+            <div v-show="!isCollapsed">
+                <filters @filters="ApplyFilters" @deleteFilters="resetFilters" :filters="filters_url" :meta="meta" />
+            </div>
+        </transition>
+    </div>
 </template>
 
 
@@ -16,13 +23,47 @@ export default {
         filters_url: Object,
         meta: Object,
     },
+
+    data() {
+        return {
+            isCollapsed: false
+        };
+    },
+
     methods: {
         ApplyFilters(filters) {
             this.$emit('apply-filters', filters);
         },
         resetFilters() {
             this.$emit('reset-filters');
+        },
+        toggleCollapse() {
+            this.isCollapsed = !this.isCollapsed;
         }
     }
 };
 </script>
+
+<style>
+.collapse-enter-active,
+.collapse-leave-active {
+    transition: max-height 0.3s ease, opacity 0.3s ease;
+    overflow: hidden;
+}
+
+.collapse-enter, .collapse-leave-to /* .collapse-leave-active en versiones anteriores */ {
+    max-height: 0;
+    opacity: 0;
+}
+
+.collapse-enter-from {
+    max-height: 0;
+    opacity: 0;
+}
+
+.collapse-leave-from {
+    max-height: 1200px; /* Ajusta según el contenido */
+    opacity: 1;
+}
+
+</style>
