d407f2c0f652037bf879b86cf3f55fdbc10ff700
Guido <gvm0898@gmail.com>
2024-11-30 03:35:46 +0100

    Frontend_Vue. Cambiada la logica de los entrenamientos al composable

--- frontend/ZenithZone/src/composables/client/useEntrenamientos.js
+++ frontend/ZenithZone/src/composables/client/useEntrenamientos.js
@@ -1,39 +1,98 @@
+import { ref, reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import entrenamientosService from '@/services/client/entrenamientos.service';
-import { ref } from 'vue';
-// import deporteService from '../../services/client/deportes.service';
 
+export function useEntrenamientos() {
+    const route = useRoute();
+    const router = useRouter();
 
-export const useEntrenamientosFilters = (filters = {}) => {
-    // console.log(filters);
-    // filters.limit = 9;
-    const entrenamientos = ref([]);
-    entrenamientosService.GetEntrenamientos(filters)
-        .then(res => { entrenamientos.value = res.data.entrenamientos; })
-        .catch(err => console.log(err));
+    let filters_url = {
+        nombre: "",
+        dia: "",
+        duracionMin: 0,
+        duracionMax: 0,
+        maxPlazasMin: 0,
+        maxPlazasMax: 0,
+        precioMin: 0,
+        precioMax: 0,
+        deporteId: "",
+        offset: 0,
+        limit: 4,
+    };
 
-    return entrenamientos;
-};
+    const state = reactive({
+        entrenamientos: [],
+        offset: filters_url.offset,
+        filters: filters_url,
+        totalPages: 0,
+        meta: {},
+    });
 
-export const useEntrenamientosPaginate = (filters_limit) => {
-    const totalPages = ref(0)
-    entrenamientosService.GetEntrenamientosData()
-        .then(res => {
-            const limit = filters_limit ?? 4;
-            const { totalEntrenamientos } = res.data;
-            const pages = Math.ceil(totalEntrenamientos / limit);
-            totalPages.value = pages;
-            console.log(`Total pages: ${pages}`);
-        })
-        .catch(error => console.error(error))
+    const fetchEntrenamientos = (filters) => {
+        entrenamientosService.GetEntrenamientos(filters)
+            .then(res => { state.entrenamientos = res.data.entrenamientos; })
+            .catch(err => console.log(err));
+    };
 
-    return totalPages;
-};
+    const fetchTotalPages = (filters_limit) => {
+        entrenamientosService.GetEntrenamientosData()
+            .then(res => {
+                const limit = filters_limit ?? 4;
+                const { totalEntrenamientos } = res.data;
+                const pages = Math.ceil(totalEntrenamientos / limit);
+                state.totalPages = pages;
+                console.log(`Total pages: ${pages}`);
+            })
+            .catch(error => console.error(error));
+    };
 
-export const useEntrenamientosData = () => {
-    const entrenamientosData = ref([])
-    entrenamientosService.GetEntrenamientosData()
-        .then(res => { entrenamientosData.value = res.data })
-        .catch(error => console.error(error))
+    const fetchEntrenamientosData = () => {
+        entrenamientosService.GetEntrenamientosData()
+            .then(res => { state.meta = res.data; })
+            .catch(error => console.error(error));
+    };
 
-    return entrenamientosData;
-};
+    const ApplyFilters = (filters) => {
+        const filters_64 = btoa(JSON.stringify(filters));
+        router.push({ name: 'serviciosEntrenamientosFilter', params: { filters: filters_64 } });
+        fetchEntrenamientos(filters);
+        fetchTotalPages(filters.limit);
+    };
+
+    const resetFilters = () => {
+        state.filters = {
+            nombre: "",
+            dia: "",
+            duracionMin: 0,
+            duracionMax: 0,
+            maxPlazasMin: 0,
+            maxPlazasMax: 0,
+            precioMin: 0,
+            precioMax: 0,
+            deporteId: "",
+            offset: 0,
+            limit: 4,
+        };
+        ApplyFilters(state.filters);
+    };
+
+    const clickCallback = (pageNum) => {
+        try {
+            if (route.params.filters !== '') {
+                filters_url = JSON.parse(atob(route.params.filters));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        filters_url.offset = pageNum - 1;
+        state.offset = pageNum;
+        ApplyFilters(filters_url);
+    };
+
+    // Initial fetch
+    fetchEntrenamientos(filters_url);
+    fetchTotalPages(filters_url.limit);
+    fetchEntrenamientosData();
+
+    return { state, filters_url, ApplyFilters, resetFilters, clickCallback };
+}
