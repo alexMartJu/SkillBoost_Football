3910ab8c4e202e5a05706e72f59478138ff6cbfe
alex <alex.mart.juan@gmail.com>
2024-12-01 22:50:17 +0100

    Backend_Springboot. Modificado PistaController para lograr scroll de todas las pistas.

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/controller/PistaController.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/controller/PistaController.java
@@ -7,6 +7,8 @@ import com.polideportivo.backend_springboot.domain.service.PistaService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 @RestController
@@ -20,7 +22,7 @@ public class PistaController {
     // Obtener todos las pistas
     @GetMapping("/pistas")
     @Transactional
-    public PistaWrapper getAllDeportes() {
+    public PistaWrapper getAllPistas() {
 
         var pistas = pistaService.getAllPistas();
         return pistaAssembler.toCollectionModel(pistas);
@@ -31,5 +33,17 @@ public class PistaController {
     public PistaResponse getBySlug(@PathVariable String slug) {
         var pista = pistaService.getBySlug(slug);
         return pistaAssembler.toResponse(pista);
+    }
+
+    // Scroll de todas las pistas
+    @GetMapping("/pistasInfinite")
+    @Transactional
+    public PistaWrapper getAllScrollPistas(
+        @RequestParam(defaultValue = "0") int offset,
+        @RequestParam(defaultValue = "4") int limit
+    ) {
+        Pageable pageable = PageRequest.of(offset, limit);
+        var pistas = pistaService.getAllScrollPistas(pageable).getContent();
+        return pistaAssembler.toCollectionModel(pistas);
     }
 }
