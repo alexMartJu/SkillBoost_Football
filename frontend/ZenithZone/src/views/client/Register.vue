ed34c6e637bd9109016c4fbdda5196c0a44247c6
Guido <gvm0898@gmail.com>
2024-12-04 20:25:38 +0100

    Frontend_Vue. Cambiado auth a login y register en router y pages

---
+++ frontend/ZenithZone/src/views/client/Register.vue
@@ -0,0 +1,21 @@
+<template>
+    <loginRegisterFormVue :isLogin="false" @send="register" />
+</template>
+
+<script>
+import loginRegisterFormVue from '../../components/auth/loginRegisterForm.vue';
+import Constant from '../../Constant';
+import { useStore } from 'vuex';
+
+export default {
+    components: { loginRegisterFormVue },
+    setup() {
+        const store = useStore();
+        const register = (data) => {
+            store.dispatch(`user/${Constant.ADD_USER}`, data);
+        }
+
+        return { register };
+    }
+}
+</script>
