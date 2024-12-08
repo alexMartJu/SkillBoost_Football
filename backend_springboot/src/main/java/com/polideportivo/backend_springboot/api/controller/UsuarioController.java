eff7b9ea2317ac13c187f630f8e7c65771d45ce8
alex <alex.mart.juan@gmail.com>
2024-12-08 21:01:45 +0100

    Backend_Springboot. Implementada la obtención del usuario actualmente logueado.

---
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.polideportivo.backend_springboot.api.controller;
+
+import com.polideportivo.backend_springboot.api.assembler.UsuarioAssembler;
+import com.polideportivo.backend_springboot.api.model.usuario.UsuarioResponse;
+import com.polideportivo.backend_springboot.api.security.authorization.CheckSecurity;
+import com.polideportivo.backend_springboot.domain.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UsuarioController {
+    
+    private final UsuarioService userService;
+    private final UsuarioAssembler userAssembler;
+
+    //Obtener el usuario actual logueado
+    @GetMapping("/user")
+    @CheckSecurity.Protected.canManage
+    public UsuarioResponse getCurrentUser() {
+        var user = userService.getCurrentUser();
+        return userAssembler.toResponse(user);
+    }
+}
