5cc70b47471f1013d338fd7e9679cab12275ac50
Rafajorda <rafajorgis@gmail.com>
2024-12-12 20:13:36 +0100

    backend_laravel añadido numeroAdmin al modelo

--- backend_laravel/app/Models/Admin.php
+++ backend_laravel/app/Models/Admin.php
@@ -22,6 +22,7 @@ class Admin extends Model  implements AuthenticatableContract, JWTSubject
     protected $fillable = [
         'nombre',
         'email',
+        'numeroAdmin',
         'password'
     ];
 
