ab1a8ecfd2d08fd7b20199af1188e5b5f9e6d698
Rafajorda <rafajorgis@gmail.com>
2024-12-12 17:41:00 +0100

    Backend_Laravel  corregido entrenador de acuerdo a los cambios pertinentes

--- backend_laravel/app/Http/Controllers/EntrenadorController.php
+++ backend_laravel/app/Http/Controllers/EntrenadorController.php
@@ -57,6 +57,10 @@ class EntrenadorController extends Controller
 
     public function register(Request $request)
     {
+        $admin = auth('admin')->user();
+        if (!$admin) {
+            return response()->json(['error' => 'Admin no encontrado'], 404);
+        }
         $request->validate([
             'nombre' => 'required|string|max:255',
             'apellidos' => 'required|string|max:255',
@@ -103,66 +107,11 @@ class EntrenadorController extends Controller
 
 
     
-    public function store(Request $request)
+
+
+    public function show($numeroEntrenador)
     {
-        
-        try {
-            
-            $request->validate([
-                'nombre' => 'required|string|max:255',
-                'apellidos' => 'required|string|max:255',
-                'DNI' => 'required|string|max:15|unique:entrenadores,DNI',
-                'email' => 'required|email|unique:entrenadores,email',
-                'password' => 'required|string|min:8',
-                'deporte_id' => 'required|integer|exists:deportes,id',
-                'edad' => 'required|integer|min:18',
-                'imagenes' => 'nullable|array', 
-                'imagenes.*' => 'string|max:255',
-            ]);
-    
-           
-            $entrenador = Entrenador::create([
-                'nombre' => $request->nombre,
-                'apellidos' => $request->apellidos,
-                'DNI' => $request->DNI,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-                'deporte_id' => $request->deporte_id,
-                'edad' => $request->edad,
-            ]);
-            if ($request->has('imagenes')) {
-                foreach ($request->input('imagenes') as $imageUrl) {
-                   
-                    $entrenador->images()->create([
-                        'image_url' => $imageUrl, 
-                    ]);
-                }
-            }
-    
-            // Retornar el entrenador creado
-            return new EntrenadoresResource($entrenador);
-    
-        } catch (\Illuminate\Validation\ValidationException $e) {
-            
-            return response()->json([
-                'error' => 'Validación fallida',
-                'messages' => $e->errors()
-            ], 422); // Código de error 422 para validación fallida
-    
-        } catch (\Exception $e) {
-            // Si ocurre cualquier otro error, devolver un mensaje genérico
-            return response()->json([
-                'error' => 'Ha ocurrido un error',
-                'message' => $e->getMessage()
-            ], 500); // Código de error 500 para errores internos
-        }
-
-    }
-
-
-    public function show($DNI)
-    {
-        $entrenador = Entrenador::where('DNI', $DNI)->firstOrFail();
+        $entrenador = Entrenador::where('numeroEntrenador', $numeroEntrenador)->firstOrFail();
         if (!$entrenador) {
             return response()->json(['error' => 'entrenador no encontrado'], 404);
         }
@@ -170,34 +119,33 @@ class EntrenadorController extends Controller
         return new EntrenadoresResource($entrenador);
     }
 
-    public function update(Request $request, $DNI)
+    public function update(Request $request)
     {
-        $entrenador = Entrenador::where('DNI', $DNI)->first();
+        $entrenador = auth('entrenador')->user();
+
         if (!$entrenador) {
             return response()->json(['error' => 'entrenador no encontrado'], 404);
         }
-        $request->validate([
-            'nombre' => 'nullable|string|max:255',
-            'apellidos' => 'nullable|string|max:255',
-            'DNI' => 'nullable|string|max:15|unique:entrenadores,DNI,' . $DNI . ',DNI',
-            'email' => 'nullable|email|unique:entrenadores,email,' . $DNI . ',DNI',
-            'password' => 'nullable|string|min:8',
-            'deporte_id' => 'nullable|integer|exists:deportes,id',
-            'edad' => 'nullable|integer|min:18',
-            'imagenes' => 'nullable|array',
-            'imagenes.*' => 'string|max:255',
-        ]);
+       $request->validate([
+        'nombre' => 'nullable|string|max:255',
+        'apellidos' => 'nullable|string|max:255',
+        'email' => 'nullable|email|unique:entrenadores,email,' . $entrenador->id, 
+        'password' => 'nullable|string|min:8',
+        'deporte_id' => 'nullable|integer|exists:deportes,id',
+        'edad' => 'nullable|integer|min:18',
+        'imagenes' => 'nullable|array',
+        'imagenes.*' => 'string|max:255',
+    ]);
 
        
-        $entrenador->update($request->only([
-            'nombre', 
-            'apellidos', 
-            'DNI', 
-            'email', 
-            'password', 
-            'deporte_id', 
-            'edad'
-        ]));
+    $entrenador->update($request->only([
+        'nombre', 
+        'apellidos', 
+        'email', 
+        'password', 
+        'deporte_id', 
+        'edad'
+    ]));
 
       
         if ($request->has('password')) {
