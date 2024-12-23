33ae211a717f542bc7a5b272400641bae7ae9bbe
Guido <gvm0898@gmail.com>
2024-12-23 18:48:19 +0100

    Frontend_Vue. Añadido el componente de carrusel al details

--- frontend/ZenithZone/src/views/client/Details.vue
+++ frontend/ZenithZone/src/views/client/Details.vue
@@ -1,28 +1,40 @@
 <template>
     <main class="main-details">
         <section class="container">
-            <h1>{{ state.entrenamientos.nombre }}</h1>
-            <div class="entrenamiento-info">
-                <p>{{ state.entrenamientos.descripcion }}</p>
-                <p>{{ state.entrenamientos.dia }}</p>
-                <p>{{ state.entrenamientos.duracion }} minutos</p>
-                <p>Plazas máximas: {{ state.entrenamientos.maxPlazas }}</p>
-                <p>Precio: {{ state.entrenamientos.precio }}€</p>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="entrenamiento-info" v-if="state.entrenamientos">
+                        <h1>{{ state.entrenamientos.nombre }}</h1>
+                        <p>{{ state.entrenamientos.descripcion }}</p>
+                        <p>{{ state.entrenamientos.dia }}</p>
+                        <p>{{ state.entrenamientos.duracion }} minutos</p>
+                        <p>Plazas máximas: {{ state.entrenamientos.maxPlazas }}</p>
+                        <p>Precio: {{ state.entrenamientos.precio }}€</p>
+                        <div>
+                            <h2>Pistas de {{ state.entrenamientos.deporte?.nombre }}</h2>
+                            <ul v-for="pista in state.entrenamientos.deporte?.pistas" :key="pista.id">
+                                <li>{{ pista.nombre }}</li>
+                            </ul>
+                        </div>
+                        <button class="btn btn-primary">Botón para las reservas</button>
+                    </div>
+                    <div v-else>
+                        <p>Cargando información de entrenamiento...</p>
+                    </div>
+                </div>
+                <!-- Carousel -->
+                <div class="col-md-6">
+                    <!-- <CarouselDetails /> -->
+                    <CarouselDetails :pistas="state.entrenamientos.deporte.pistas"/>
+                </div>
             </div>
-
-            <div>
-                <h2>Pistas de {{ state.entrenamientos.deporte.nombre }}</h2>
-                <ul v-for="pista in state.entrenamientos.deporte.pistas">
-                    <li>{{ pista.nombre }}</li>
-                </ul>
-            </div>
-
-            <button class="btn btn-primary">Botón para las reservas</button>
         </section>
     </main>
 </template>
 
+
 <script>
+import CarouselDetails from '@/components/shared/carouselDetails.vue';
 import Constant from '@/Constant';
 import { computed } from 'vue';
 import { reactive } from 'vue';
@@ -30,6 +42,10 @@ import { useRoute, useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 
 export default {
+    components: {
+        CarouselDetails
+    },
+
     setup() {
         const store = useStore();
         const route = useRoute();
