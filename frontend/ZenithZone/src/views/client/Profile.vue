739c076625d119b9405b13b4d2c81c62a45613e4
Guido <gvm0898@gmail.com>
2024-12-24 20:28:51 +0100

    Frontend_Vue. Estructura básica y lógica de la page Profile

---
+++ frontend/ZenithZone/src/views/client/Profile.vue
@@ -0,0 +1,60 @@
+<template>
+    <main class="main-profile">
+        <div class="container">
+            <ProfileInfo :profile="state.Profile" :isCurrentUser="isCurrentUser"/>
+        </div>
+        <ProfileNav :profile="state.Profile" :isCurrentUser="isCurrentUser" />
+    </main>
+
+</template>
+
+<script>
+import ProfileInfo from '@/components/profile/ProfileInfo.vue';
+import ProfileNav from '@/components/profile/ProfileNav.vue';
+import Constant from '@/Constant';
+import { reactive, computed, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+
+export default {
+    name: 'Profile',
+
+    components: {
+        ProfileInfo,
+        ProfileNav
+    },
+
+    setup() {
+        const router = useRouter();
+        const route = useRoute();
+        const store = useStore();
+
+        const fetchData = () => {
+            store.dispatch(`user/${Constant.INITIALIZE_PROFILE}`, route.params.numeroSocio)
+        };
+
+        const state = reactive({
+            Profile: computed(() => store.getters['user/GetProfile']),
+            CurrentUser: computed(() => store.getters['user/GetCurrentUser']),
+        });
+
+        let isCurrentUser = false
+        if (state.Profile.numeroSocio === state.CurrentUser.numeroSocio) {
+            isCurrentUser = true;
+        }
+
+        watch(
+            () => [route.params.numeroSocio],
+            fetchData(),
+        );
+
+        return { isCurrentUser, state };
+    },
+};
+</script>
+
+<style>
+    .main-profile {
+        margin-top: 50px;
+    }
+</style>
