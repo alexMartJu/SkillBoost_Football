3e4b8fbab4388f357713f53a5a3edd2977e007d0
Guido <gvm0898@gmail.com>
2024-12-26 01:20:55 +0100

    Frontend_Vue. Componente entrenamientos perfil

--- frontend/ZenithZone/src/components/profile/ProfileEntrenamientos.vue
+++ frontend/ZenithZone/src/components/profile/ProfileEntrenamientos.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>entrenamientos</h1>
+    <div class="row justify-content-center">
+        <CardEntrenamientos 
+            v-for="entrenamiento in state.entrenamientos" 
+            :key="entrenamiento.id" 
+            :entrenamiento="entrenamiento" 
+            class="col-md-6" />
+    </div>
 </template>
+
+<script>
+import { reactive } from 'vue';
+import CardEntrenamientos from '../shared/CardEntrenamientos.vue';
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+import Constant from '@/Constant';
+import { useRoute } from 'vue-router';
+
+export default {
+    components: {
+        CardEntrenamientos,
+    },
+    
+    setup() {
+        const store = useStore();
+        const route = useRoute();
+
+        store.dispatch(`profile/${Constant.INITIALIZE_ENTRENAMIENTO_PROFILE}`);
+        // store.dispatch(`profile/${Constant.INITIALIZE_ENTRENAMIENTO_PROFILE}`, route.params.numeroSocio);
+
+        const state = reactive({
+            entrenamientos: computed(() => store.getters['profile/GetEntrenamientosProfile'])
+            // entrenamientos: computed(() => store.getters['entrenamientos/GetEntrenamientos'])
+        });
+
+        return { state };
+    }
+};
+</script>
