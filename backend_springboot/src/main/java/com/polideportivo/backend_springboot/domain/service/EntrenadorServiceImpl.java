825592191c6e02e19858589b8dde61154a1c63c4
alex <alex.mart.juan@gmail.com>
2024-12-27 10:24:27 +0100

    Backend_Springboot. Creado EntrenadorService y EntrenadorServiceImpl para obtener entrenadores no eliminados y asignar imágenes, con modificación en EntrenadorRepository para filtrar por 'deletedAt'.

---
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/domain/service/EntrenadorServiceImpl.java
@@ -0,0 +1,30 @@
+package com.polideportivo.backend_springboot.domain.service;
+
+import com.polideportivo.backend_springboot.domain.model.Entrenador;
+import com.polideportivo.backend_springboot.domain.repository.EntrenadorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EntrenadorServiceImpl implements EntrenadorService {
+
+    private final EntrenadorRepository repository;
+    private final ImageService imageService;
+    
+    @Transactional(readOnly = true)
+    public List<Entrenador> getAllEntrenadores() {
+
+        // Obtiene todos los entrenadores no eliminados
+        List<Entrenador> entrenadores = repository.findByDeletedAtIsNull();
+        
+        // Asigna imágenes a cada entrenador
+        entrenadores.forEach(entrenador -> entrenador.setImages(
+            imageService.getImagesForEntity("App\\Models\\Entrenador", entrenador.getId())
+        ));
+        return entrenadores;
+    }
+}
