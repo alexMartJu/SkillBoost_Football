c57d39f727d0f71e65cd8f47bde707d0f25861f5
GuiVelMat <gvm0898@gmail.com>
2024-12-30 22:40:43 +0100

    Frontend_Vue. WatchEffect para actualizar cunado te quitas de un entrenamiento

--- frontend/ZenithZone/src/components/profile/ProfileEntrenamientos.vue
+++ frontend/ZenithZone/src/components/profile/ProfileEntrenamientos.vue
@@ -1,37 +1,33 @@
 <template>
     <div class="row justify-content-center">
-        <CardEntrenamientos 
-            v-for="entrenamiento in state.entrenamientos" 
-            :key="entrenamiento.id" 
-            :entrenamiento="entrenamiento"
-            :isProfile="true"
-            class="col-md-6" />
+        <CardEntrenamientos v-for="entrenamiento in state.entrenamientos" :key="entrenamiento.id"
+            :entrenamiento="entrenamiento" :isProfile="true" class="col-md-6" />
     </div>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { reactive, watchEffect } from 'vue';
 import CardEntrenamientos from '../shared/CardEntrenamientos.vue';
 import { useStore } from 'vuex';
 import { computed } from 'vue';
 import Constant from '@/Constant';
-import { useRoute } from 'vue-router';
 
 export default {
     components: {
         CardEntrenamientos,
     },
-    
+
     setup() {
         const store = useStore();
-        const route = useRoute();
 
         store.dispatch(`profile/${Constant.INITIALIZE_ENTRENAMIENTO_PROFILE}`);
-        // store.dispatch(`profile/${Constant.INITIALIZE_ENTRENAMIENTO_PROFILE}`, route.params.numeroSocio);
 
         const state = reactive({
             entrenamientos: computed(() => store.getters['profile/GetEntrenamientosProfile'])
-            // entrenamientos: computed(() => store.getters['entrenamientos/GetEntrenamientos'])
+        });
+
+        watchEffect(() => {
+            store.dispatch(`profile/${Constant.INITIALIZE_ENTRENAMIENTO_PROFILE}`);
         });
 
         return { state };
