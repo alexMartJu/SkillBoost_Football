c27765aeb1fd99c292a5ebaa733330437819335c
alex <alex.mart.juan@gmail.com>
2024-12-31 00:44:25 +0100

    Backend_Springboot. Actualizar EntrenamientoResponse para incluir PistaPrivada y modificar EntrenamientoAssembler para gestionar la relación con PistaPrivada, incluyendo imágenes asociadas

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/assembler/EntrenamientoAssembler.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/assembler/EntrenamientoAssembler.java
@@ -1,10 +1,14 @@
 package com.polideportivo.backend_springboot.api.assembler;
 
 import com.polideportivo.backend_springboot.api.model.entrenamiento.EntrenamientoWrapper;
+import com.polideportivo.backend_springboot.api.model.pistaPrivada.PistaPrivadaResponse;
 import com.polideportivo.backend_springboot.api.model.entrenamiento.EntrenamientoResponse;
 import com.polideportivo.backend_springboot.api.model.entrenamiento.EntrenamientoCountResponse;
 import com.polideportivo.backend_springboot.api.model.entrenamiento.EntrenamientoDataResponse;
 import com.polideportivo.backend_springboot.domain.model.Entrenamiento;
+import com.polideportivo.backend_springboot.domain.model.Image;
+import com.polideportivo.backend_springboot.domain.service.ImageService;
+
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -15,9 +19,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EntrenamientoAssembler {
     private final ModelMapper modelMapper;
+    private final PistaPrivadaAssembler pistaPrivadaAssembler;
+    private final ImageService imageService;
 
     public EntrenamientoResponse toResponse(Entrenamiento entrenamiento) {
         var response = modelMapper.map(entrenamiento, EntrenamientoResponse.class);
+
+        // if (entrenamiento.getPistaPrivada() != null) {
+        //     PistaPrivadaResponse pistaPrivadaResponse = pistaPrivadaAssembler.toResponse(entrenamiento.getPistaPrivada());
+        //     response.setPistaPrivada(pistaPrivadaResponse);  // Asignamos la pista privada al DTO
+        // }
+        // Verificamos si el entrenamiento tiene una pista privada asociada
+        if (entrenamiento.getPistaPrivada() != null) {
+            PistaPrivadaResponse pistaPrivadaResponse = pistaPrivadaAssembler.toResponse(entrenamiento.getPistaPrivada());
+
+            // Obtener las imágenes asociadas a la pista privada
+            List<Image> images = imageService.getImagesForEntity("App\\Models\\PistaPrivada", entrenamiento.getPistaPrivada().getId());
+
+            // Establecemos las imágenes en el DTO de la pista privada
+            pistaPrivadaResponse.setImages(images);
+
+            // Asignamos la pista privada con sus imágenes al DTO de entrenamiento
+            response.setPistaPrivada(pistaPrivadaResponse);
+        }
+
         return response;
     }
 
