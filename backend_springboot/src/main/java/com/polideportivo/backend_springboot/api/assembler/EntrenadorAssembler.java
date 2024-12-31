36f96e4c229799c24b588e4ad768667cacb9dfd0
alex <alex.mart.juan@gmail.com>
2024-12-31 17:04:52 +0100

    Backend_Springboot. Crear DTO EntrenadorInfoResponse y agregar método toInfoResponse en EntrenadorAssembler para mapear los datos del entrenador, incluyendo sus imágenes

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/assembler/EntrenadorAssembler.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/assembler/EntrenadorAssembler.java
@@ -2,8 +2,12 @@ package com.polideportivo.backend_springboot.api.assembler;
 
 import com.polideportivo.backend_springboot.api.model.entrenador.EntrenadorWrapper;
 import com.polideportivo.backend_springboot.api.model.deporte.DeporteResponse;
+import com.polideportivo.backend_springboot.api.model.entrenador.EntrenadorInfoResponse;
 import com.polideportivo.backend_springboot.api.model.entrenador.EntrenadorResponse;
 import com.polideportivo.backend_springboot.domain.model.Entrenador;
+import com.polideportivo.backend_springboot.domain.model.Image;
+import com.polideportivo.backend_springboot.domain.service.ImageService;
+
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -16,6 +20,7 @@ public class EntrenadorAssembler {
     
     private final ModelMapper modelMapper;
     private final SalaAssembler salaAssembler;
+    private final ImageService imageService;
 
     public EntrenadorResponse toResponse(Entrenador entrenador) {
         var response = modelMapper.map(entrenador, EntrenadorResponse.class);
@@ -44,6 +49,18 @@ public class EntrenadorAssembler {
     private EntrenadorWrapper buildResponse(List<EntrenadorResponse> entrenadores) {
         return EntrenadorWrapper.builder()
                 .entrenadores(entrenadores)
+                .build();
+    }
+
+    public EntrenadorInfoResponse toInfoResponse(Entrenador entrenador) {
+        // Obtener las imágenes asociadas al entrenador
+        List<Image> entrenadorImages = imageService.getImagesForEntity("App\\Models\\Entrenador", entrenador.getId());
+
+        // Crear y devolver el DTO con la información del entrenador y sus imágenes
+        return EntrenadorInfoResponse.builder()
+                .nombre(entrenador.getNombre())
+                .apellidos(entrenador.getApellidos())
+                .images(entrenadorImages != null ? entrenadorImages : List.of()) // Si no hay imágenes, asignamos una lista vacía
                 .build();
     }
 }
