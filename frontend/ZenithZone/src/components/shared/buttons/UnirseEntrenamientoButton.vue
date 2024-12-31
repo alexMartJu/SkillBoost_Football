20ca6a2f757943389c0c85a4ed37e34ea196129b
GuiVelMat <gvm0898@gmail.com>
2024-12-31 01:28:57 +0100

    Frontend_Vue. Componente botón Apuntarse entrenamiento

---
+++ frontend/ZenithZone/src/components/shared/buttons/UnirseEntrenamientoButton.vue
@@ -0,0 +1,58 @@
+<template>
+    <button class="btn btn-success" @click="unirseEntrenamiento(slug)" :disabled="!state.isLogged">Apuntarse</button>
+</template>
+
+<script>
+import profileService from '@/services/client/profile.service';
+import Swal from 'sweetalert2';
+import { computed } from 'vue';
+import { reactive } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+    props: {
+        slug: {
+            type: String,
+            Required: true
+        },
+    },
+
+    setup() {
+        const store = useStore();
+
+        const state = reactive({
+            isLogged: computed(() => store.getters['user/GetIsAuth']),
+        });
+
+        const unirseEntrenamiento = async (slug) => {
+            const result = await Swal.fire({
+                title: '¿Quieres apuntarte a este entrenamiento?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí'
+            });
+
+            if (result.isConfirmed) {
+                try {
+                    await profileService.UnirseEntrenamiento(slug);
+                    Swal.fire(
+                        'Unido',
+                        'Te has apuntado al entrenamiento.',
+                        'success'
+                    );
+                } catch (e) {
+                    Swal.fire(
+                        'Error',
+                        'Ya estás apuntado al entrenamiento.',
+                        'error'
+                    );
+                }
+            }
+        }
+
+        return { state, unirseEntrenamiento }
+    }
+}
+</script>
