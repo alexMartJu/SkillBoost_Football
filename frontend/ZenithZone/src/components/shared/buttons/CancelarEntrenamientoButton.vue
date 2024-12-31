48946566541307347a05acce77baf6b1e3c13580
GuiVelMat <gvm0898@gmail.com>
2024-12-31 01:28:50 +0100

    Frontend_Vue. Componente botón Cancelar entrenamiento

---
+++ frontend/ZenithZone/src/components/shared/buttons/CancelarEntrenamientoButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <button class="btn btn-danger" @click="cancelarEntrenamiento(slug)">Cancelar</button>
+</template>
+
+<script>
+import Constant from '@/Constant';
+import profileService from '@/services/client/profile.service';
+import Swal from 'sweetalert2';
+
+export default {
+    props: {
+        slug: {
+            type: String,
+            Required: true
+        },
+    },
+
+    setup() {
+        const cancelarEntrenamiento = async (slug) => {
+            const result = await Swal.fire({
+                title: '¿Quieres dejar el entrenamiento?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí'
+            });
+
+            if (result.isConfirmed) {
+                await profileService.CancelarEntrenamiento(slug);
+                Swal.fire(
+                    'Cancelado',
+                    'Tu entrenamiento ha sido cancelado.',
+                    'success'
+                );
+                store.dispatch(`profile/${Constant.INITIALIZE_ENTRENAMIENTO_PROFILE}`);
+            }
+        }
+
+        return { cancelarEntrenamiento }
+    }
+}
+</script>
