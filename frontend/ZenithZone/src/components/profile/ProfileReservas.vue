d0ac9dfcdbaa21c482d1681c885f619d4b9c8cb3
GuiVelMat <gvm0898@gmail.com>
2025-01-04 17:19:18 +0100

    Frontend_Vue. Recarga correcta del componente ProfileReservas

--- frontend/ZenithZone/src/components/profile/ProfileReservas.vue
+++ frontend/ZenithZone/src/components/profile/ProfileReservas.vue
@@ -1,15 +1,18 @@
 <template>
+    <p>{{ }}</p>
     <div class="row g-4 justify-content-center">
-        <CardReservas v-for="reserva in state.reservas" :key="reserva.id" :reserva="reserva" class="col-md-6"/>
+        <div v-for="reservas in state.reservas" :reservas="reservas" class="col-md-6">
+            <CardReservas v-for="reserva in reservas.reservas" :key="reserva.id" :reserva="reserva"
+                :slug="reservas.slug" />
+        </div>
     </div>
 </template>
 
 <script>
 import { useStore } from 'vuex';
 import CardReservas from '../shared/CardReservas.vue';
-import { useRoute } from 'vue-router';
 import Constant from '@/Constant';
-import { reactive } from 'vue';
+import { reactive, watchEffect } from 'vue';
 import { computed } from 'vue';
 
 export default {
@@ -19,23 +22,14 @@ export default {
 
     setup() {
         const store = useStore();
-        const route = useRoute();
 
-        // store.dispatch(`profile/${Constant.INITIALIZE_RESERVA_PROFILE}`, route.params.numeroSocio);
-
-        // const state = reactive({
-        //     reservas: computed(() => store.getters['profile/GetReservasProfile'])
-        // });
-
-        // return { state }
-
-        // =================================================
-
-        store.dispatch(`profile/${Constant.INITIALIZE_ENTRENAMIENTO_PROFILE}`);
+        store.dispatch(`profile/${Constant.INITIALIZE_RESERVA_PROFILE}`);
 
         const state = reactive({
-            reservas: computed(() => store.getters['profile/GetEntrenamientosProfile'])
+            reservas: computed(() => store.getters['profile/GetReservasProfile'])
         });
+
+        watchEffect(() => { });
 
         return { state };
     }
