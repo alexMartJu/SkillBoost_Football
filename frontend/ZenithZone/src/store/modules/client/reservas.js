dcc77516941dfdc176b1d3c85da70c86a6294a75
GuiVelMat <gvm0898@gmail.com>
2025-01-04 17:18:05 +0100

    Frontend_Vue. Store reservas

---
+++ frontend/ZenithZone/src/store/modules/client/reservas.js
@@ -0,0 +1,55 @@
+import pistaService from '../../../services/client/pistas.service';
+import Constant from '../../../Constant';
+
+export const reservas = {
+    namespaced: true,
+
+    state: {
+        pistas: []
+    },
+
+    actions: {
+        [Constant.INITIALIZE_RESERVA]: async (store) => {
+            try {
+                const { data } = await pistaService.GetPistas();
+                // console.log(data);
+
+                store.commit(Constant.INITIALIZE_PISTA, data.pistas);
+            } catch (error) {
+                console.error("Error al cargar las pistas:", error);
+            }
+        },
+
+        [Constant.DELETE_ONE_RESERVA]: async (store, slug) => {
+            console.log(`slug: ${slug}`);
+            try {
+                const { data } = await pistaService.GetOnePista(slug);
+                store.commit(Constant.INITIALIZE_ONE_STATE_PISTA, data);
+            } catch (error) {
+                console.error("Error al cargar la pista:", error);
+            }
+        },
+    },
+
+    mutations: {
+        [Constant.INITIALIZE_PISTA](state, payload) {
+            if (payload) {
+                state.pistas = payload;
+            }
+        },
+        [Constant.INITIALIZE_ONE_STATE_PISTA](state, payload) {
+            if (payload) {
+                state.pistas = payload;
+            }
+        },
+    },
+
+    getters: {
+        GetPistas(state) {
+            return state.pistas;
+        },
+        GetOnePista(state) {
+            return state.pistas;
+        }
+    }
+};
