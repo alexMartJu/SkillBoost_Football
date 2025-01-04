662e20aacfbb5ffc808ee895ed7a5c18711786cf
alex <alex.mart.juan@gmail.com>
2025-01-04 08:55:41 +0100

    Backend_Springboot. Añadido endpoint para obtener las pistas reservadas del usuario logueado en PistaController

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/controller/PistaController.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/controller/PistaController.java
@@ -1,9 +1,11 @@
 package com.polideportivo.backend_springboot.api.controller;
 
 import com.polideportivo.backend_springboot.api.assembler.PistaAssembler;
+import com.polideportivo.backend_springboot.api.model.pista.PistaReservadaResponse;
 import com.polideportivo.backend_springboot.api.model.pista.PistaResponse;
 import com.polideportivo.backend_springboot.api.model.pista.PistaWrapper;
 import com.polideportivo.backend_springboot.api.security.authorization.CheckSecurity;
+import com.polideportivo.backend_springboot.domain.model.Pista;
 import com.polideportivo.backend_springboot.domain.service.PistaService;
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -49,5 +52,15 @@ public class PistaController {
         Pageable pageable = PageRequest.of(offset, limit);
         var pistas = pistaService.getAllScrollPistas(pageable).getContent();
         return pistaAssembler.toCollectionModel(pistas);
+    }
+
+    // Endpoint para ver pistas reservas de usuario logueado en el perfil
+    @GetMapping("/profilePistas")
+    @CheckSecurity.Protected.canManage
+    public List<PistaReservadaResponse> getProfilePistas() {
+        // Obtiene las pistas reservadas como entidades de dominio
+        List<Pista> pistasReservadas = pistaService.getPistasReservadasCurrentUser();
+        // Convierte las entidades de dominio en DTOs utilizando el assembler
+        return pistaAssembler.toPistaReservadaResponseList(pistasReservadas);
     }
 }
