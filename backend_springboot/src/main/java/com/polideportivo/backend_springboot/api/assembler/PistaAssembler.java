560f2421ecbc8901fce1da447a8fd105b64a1f5c
alex <alex.mart.juan@gmail.com>
2025-01-04 08:53:38 +0100

    Backend_Springboot. Añadida DTO PistaReservadaResponse y método para mapear pistas con reservas en PistaAssembler

--- backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/assembler/PistaAssembler.java
+++ backend_springboot/src/main/java/com/polideportivo/backend_springboot/api/assembler/PistaAssembler.java
@@ -1,13 +1,18 @@
 package com.polideportivo.backend_springboot.api.assembler;
 
 import com.polideportivo.backend_springboot.api.model.pista.PistaWrapper;
+import com.polideportivo.backend_springboot.api.model.reserva.ReservaResponse;
+import com.polideportivo.backend_springboot.api.model.pista.PistaReservadaResponse;
 import com.polideportivo.backend_springboot.api.model.pista.PistaResponse;
 import com.polideportivo.backend_springboot.domain.model.Pista;
+import com.polideportivo.backend_springboot.domain.model.Reserva;
+
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -35,5 +40,34 @@ public class PistaAssembler {
         return PistaWrapper.builder()
                 .pistas(pistas)
                 .build();
+    }
+
+    public PistaReservadaResponse toPistaReservadaResponse(Pista pista) {
+        // Mapeo básico de la información de la pista
+        PistaReservadaResponse response = modelMapper.map(pista, PistaReservadaResponse.class);
+
+        // Añade las reservas asociadas a la pista
+        List<ReservaResponse> reservas = pista.getReservas().stream()
+                .map(this::toReservaResponse)
+                .collect(Collectors.toList());
+
+        response.setReservas(reservas);
+        return response;
+    }
+
+    // Convierte una lista de entidades Pista en una lista de DTOs PistaReservadaResponse
+    public List<PistaReservadaResponse> toPistaReservadaResponseList(List<Pista> pistas) {
+        return pistas.stream()
+                .map(this::toPistaReservadaResponse)
+                .collect(Collectors.toList());
+    }
+
+    // Convierte una entidad Reserva en un DTO ReservaResponse
+    private ReservaResponse toReservaResponse(Reserva reserva) {
+        ReservaResponse response = new ReservaResponse();
+        response.setPistaNombre(reserva.getPista().getNombre());
+        response.setHora(reserva.getHorario().getHora());
+        response.setFecha(reserva.getFecha().toString());
+        return response;
     }
 }
