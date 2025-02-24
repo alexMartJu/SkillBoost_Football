89d934ab037601fb844c3e38ec526117a92f590d
alexMartJu <alex.mart.juan@gmail.com>
2025-02-24 19:44:25 +0100

    Backend_Main_SpringBoot. Estructura inicial de archivos de SpringBoot.

---
+++ backend_main_springboot/src/test/java/com/skillboostfootball/backend_main_springboot/BackendMainSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillboostfootball.backend_main_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMainSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
