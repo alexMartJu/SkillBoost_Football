1ef8cb5506778eae0450b4e81ddc7b5286877d7b
alexMartJu <alex.mart.juan@gmail.com>
2025-03-02 00:17:55 +0100

    Backend_Main_Springboot. Implementados use cases para Pistas (listar, crear, modificar y eliminar)

---
+++ backend_main_springboot/src/main/java/com/skillboostfootball/backend_main_springboot/application/useCases/pistas/CreatePistaUseCase.java
@@ -0,0 +1,38 @@
+package com.skillboostfootball.backend_main_springboot.application.useCases.pistas;
+
+import com.skillboostfootball.backend_main_springboot.application.applicationServices.SlugService;
+import com.skillboostfootball.backend_main_springboot.domain.entities.pistas.Pista;
+import com.skillboostfootball.backend_main_springboot.domain.repositories.pistas.PistaRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CreatePistaUseCase {
+    
+    private final PistaRepository pistaRepository;
+    private final SlugService slugService;
+    
+    @Transactional
+    public Pista execute(String nombre, String descripcion, String tipo, String dimensiones, Integer capacidad) {
+        String slug = slugService.generateSlug(nombre);
+        
+        Pista pista = Pista.builder()
+            .nombre(nombre)
+            .descripcion(descripcion)
+            .slug(slug)
+            .tipo(tipo)
+            .dimensiones(dimensiones)
+            .capacidad(capacidad)
+            .images(new ArrayList<>())
+            .createdAt(LocalDateTime.now())
+            .updatedAt(LocalDateTime.now())
+            .build();       
+        return pistaRepository.save(pista);
+    }
+
+}
---
+++ backend_main_springboot/src/main/java/com/skillboostfootball/backend_main_springboot/application/useCases/pistas/DeletePistaUseCase.java
@@ -0,0 +1,27 @@
+package com.skillboostfootball.backend_main_springboot.application.useCases.pistas;
+
+import com.skillboostfootball.backend_main_springboot.domain.exceptions.pistas.PistaNotFoundException;
+import com.skillboostfootball.backend_main_springboot.domain.entities.pistas.Pista;
+import com.skillboostfootball.backend_main_springboot.domain.repositories.pistas.PistaRepository;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DeletePistaUseCase {
+    
+    private final PistaRepository pistaRepository;
+    
+    @Transactional
+    public void execute(String slug) {
+        Pista pista = pistaRepository.findBySlug(slug).orElseThrow(PistaNotFoundException::new);
+        
+        // Marcar como eliminada (soft delete) a través del método de domain
+        pista.delete();
+        
+        pistaRepository.delete(pista);
+    }
+
+}
