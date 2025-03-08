838a6ee1b0b1264598ba8837e16f52f8f1a7a3f9
alexMartJu <alex.mart.juan@gmail.com>
2025-03-08 15:42:45 +0100

    Backend_Main_Springboot. Añadidas entities de persistencia para seguridad y autorización (Role, Permission y Blacklist)

---
+++ backend_main_springboot/src/main/java/com/skillboostfootball/backend_main_springboot/infrastructure/databases/entities/roles/RoleEntity.java
@@ -0,0 +1,47 @@
+package com.skillboostfootball.backend_main_springboot.infrastructure.databases.entities.roles;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.skillboostfootball.backend_main_springboot.infrastructure.databases.entities.permissions.PermissionEntity;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "slug", unique = true, nullable = false)
+    private String slug;
+
+    @Column(name = "description")
+    private String description;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "role_permissions",
+        joinColumns = @JoinColumn(name = "role_id"),
+        inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private List<PermissionEntity> permissions;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
