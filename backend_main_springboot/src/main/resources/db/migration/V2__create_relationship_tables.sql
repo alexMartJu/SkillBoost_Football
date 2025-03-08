be91baa4f8d8e8f7de27edeb3ad83b046a32ff61
alexMartJu <alex.mart.juan@gmail.com>
2025-03-08 12:32:29 +0100

    Backend_Main_SpringBoot. Corregidas y modificadas tablas de migración relacionadas con Usuarios, Roles y Permisos para cambiar id por bigint

--- backend_main_springboot/src/main/resources/db/migration/V2__create_relationship_tables.sql
+++ backend_main_springboot/src/main/resources/db/migration/V2__create_relationship_tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE role_permissions (
-    role_id int,
-    permission_id int,
+    role_id bigint,
+    permission_id bigint,
     PRIMARY KEY (role_id, permission_id),
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (permission_id) REFERENCES permissions(id)
 );
 
 CREATE TABLE user_roles (
-    user_id int,
-    role_id int,
+    user_id bigint,
+    role_id bigint,
     PRIMARY KEY (user_id, role_id),
     FOREIGN KEY (user_id) REFERENCES usuarios(id),
     FOREIGN KEY (role_id) REFERENCES roles(id)
@@ -16,8 +16,8 @@ CREATE TABLE user_roles (
 
 CREATE TABLE blacklist_token (
     id bigint PRIMARY KEY AUTO_INCREMENT,
-    usuario_id int NOT NULL,
-    refresh_token varchar(255) NOT NULL,
+    usuario_id bigint NOT NULL,
+    refresh_token TEXT NOT NULL,
     revoke_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
