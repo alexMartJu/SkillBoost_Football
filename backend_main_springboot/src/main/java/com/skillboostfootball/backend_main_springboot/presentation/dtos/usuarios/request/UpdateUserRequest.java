c91d7c3f0e08910e42361427928582983fd884bd
alexMartJu <alex.mart.juan@gmail.com>
2025-03-08 12:56:36 +0100

    Backend_Main_Springboot. Añadidas DTOs y assemblers de Usuarios

---
+++ backend_main_springboot/src/main/java/com/skillboostfootball/backend_main_springboot/presentation/dtos/usuarios/request/UpdateUserRequest.java
@@ -0,0 +1,31 @@
+package com.skillboostfootball.backend_main_springboot.presentation.dtos.usuarios.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateUserRequest {
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+    
+    @NotBlank(message = "Los apellidos son obligatorios")
+    private String apellidos;
+    
+    private String bio;
+    
+    private String image;
+    
+    @Email(message = "El formato de email es inválido")
+    private String email;
+    
+    private String password;
+    
+    @Positive(message = "La edad debe ser un número positivo")
+    private Integer edad;
+    
+    private String posicionPreferida;
+}
