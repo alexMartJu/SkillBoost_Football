97f0f50ba5c6982a3efe39807ce16211ab1e6563
alexMartJu <alex.mart.juan@gmail.com>
2025-03-14 23:13:03 +0100

    Backend_Main_Springboot. Añadido soporte de paginación en repositorios de SubtipoTecnificacion

--- backend_main_springboot/src/main/java/com/skillboostfootball/backend_main_springboot/infrastructure/repositoryImpl/subtiposTecnificacion/SpringDataSubtipoTecnificacionRepository.java
+++ backend_main_springboot/src/main/java/com/skillboostfootball/backend_main_springboot/infrastructure/repositoryImpl/subtiposTecnificacion/SpringDataSubtipoTecnificacionRepository.java
@@ -2,11 +2,13 @@ package com.skillboostfootball.backend_main_springboot.infrastructure.repository
 
 import com.skillboostfootball.backend_main_springboot.infrastructure.databases.entities.subtiposTecnificacion.SubtipoTecnificacionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
 public interface SpringDataSubtipoTecnificacionRepository extends JpaRepository<SubtipoTecnificacionEntity, Long> {
     List<SubtipoTecnificacionEntity> findByDeletedAtIsNull();
     Optional<SubtipoTecnificacionEntity> findBySlugAndDeletedAtIsNull(String slug);
+    Page<SubtipoTecnificacionEntity> findByDeletedAtIsNull(Pageable pageable);
 }
